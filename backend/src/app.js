2781c1b3429b711bb26ba54178636149f88275df
zuhairzaidi63 <zuhair.zaidi407@gmail.com>
2024-04-27 16:19:01 +0500

    add backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
+
 const path = require('path');
 
-const p = path.join(__dirname,"../public");
+const p = path.join(__dirname,"../views");
 app.use(express.static(p));
+
+app.set("view engine","ejs")
 
 require("./db/conn");
 app.get("/",(req,res) =>{
-    res.send("yes");
+    res.render("index");
+    // res.send("yes")
 })
 app.listen(3000);
